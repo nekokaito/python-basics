56e48bea7bb08ebb8b008925b315d1e4fac526be
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-07-06 12:33:41 +0600

    loop

---
+++ Loop.py
@@ -0,0 +1,13 @@
+myGames = ['dmc', 'valorant', 'fifa']
+
+
+# It will count of all list
+
+for i in myGames: 
+     print(i)
+
+# It will set a limit or range
+
+for i in range(len(myGames)):
+     print(myGames[i])
+     
